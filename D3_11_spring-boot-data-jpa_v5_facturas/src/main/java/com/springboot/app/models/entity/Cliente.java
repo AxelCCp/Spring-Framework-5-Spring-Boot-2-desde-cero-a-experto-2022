a06d9ac16a9d9c0c1c9c39d50315bdb70f6293f5
msi <hpmajin@outlook.es>
2022-04-30 22:57:01 -0400

    hasta la clase 162

---
+++ D3_11_spring-boot-data-jpa_v5_facturas/src/main/java/com/springboot/app/models/entity/Cliente.java
@@ -0,0 +1,113 @@
+package com.springboot.app.models.entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name="clientes")
+public class Cliente implements Serializable {
+
+	//CLASE158
+	public Cliente() {
+		facturas = new ArrayList<Factura>();
+	}
+	
+	//------
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getCreateAt() {
+		return createAt;
+	}
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	public String getFoto() {
+		return foto;
+	}
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	//-----CLASE 158
+	public List<Factura> getFacturas() {
+		return facturas;
+	}
+	public void setFacturas(List<Factura> facturas) {
+		this.facturas = facturas;
+	}
+
+	public void addFactura(Factura factura) {
+		facturas.add(factura);
+	}
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	@NotEmpty
+	private String nombre;
+	@NotEmpty
+	private String apellido;
+	@NotEmpty
+	@Email
+	private String email;
+	@NotNull
+	@Column(name="create_at")
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date createAt;
+	
+	private String foto; 
+	
+	//cascade= CascadeType.ALL: QUIERE DECIR QUE LAS OPERACIONES COMO PERSIST Y DELETE SE VAN A EFECTUAR EN CADENA.
+	//mappedBy : SE ASIGNA EL NOMBRE DEL ATRIBUTO DE LA CLASE CLIENTE QUE ESTÁ EN LA CLASE FACTURA. EN ESTE CASO "cliente".
+	@OneToMany(mappedBy="cliente", fetch=FetchType.LAZY, cascade= CascadeType.ALL) 
+	private List<Factura>facturas;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+}
---
+++ D3_11_spring-boot-data-jpa_v5_facturas/src/main/resources/import.sql
@@ -0,0 +1,26 @@
+INSERT INTO clientes (id, nombre, apellido, email, create_at, foto) VALUES(1, 'AAJDHF', 'ASDA', 'ASDAS1D@JHG.COM', '2022-08-28', '');
+INSERT INTO clientes (id, nombre, apellido, email, create_at, foto) VALUES(2, 'AAJDHF', 'ASDA', 'ASDASD2@JHG.COM', '2022-08-28', '');
+INSERT INTO clientes (id, nombre, apellido, email, create_at, foto) VALUES(3, 'AAJDHF', 'ASDA', 'ASDAeSD@JHG.COM', '2022-08-28', '');
+INSERT INTO clientes (id, nombre, apellido, email, create_at, foto) VALUES(4, 'AAJDHF', 'ASDA', 'ASDASdD@JHG.COM', '2022-08-28', '');
+INSERT INTO clientes (id, nombre, apellido, email, create_at, foto) VALUES(5, 'AAJDHF', 'ASDA', 'ASDASfD@JHG.COM', '2022-08-28', '');
+INSERT INTO clientes (id, nombre, apellido, email, create_at, foto) VALUES(6, 'AAJDHF', 'ASDA', 'ASDASgD@JHG.COM', '2022-08-28', '');
+INSERT INTO clientes (id, nombre, apellido, email, create_at, foto) VALUES(7, 'AAJDHF', 'ASDA', 'ASDAShD@JHG.COM', '2022-08-28', '');
+INSERT INTO clientes (id, nombre, apellido, email, create_at, foto) VALUES(8, 'AAJDHF', 'ASDA', 'ASDASkD@JHG.COM', '2022-08-28', '');
+INSERT INTO clientes (id, nombre, apellido, email, create_at, foto) VALUES(9, 'AAJDHF', 'ASDA', 'ASDAlSD@JHG.COM', '2022-08-28', '');
+INSERT INTO clientes (id, nombre, apellido, email, create_at, foto) VALUES(10, 'AAJDHF', 'ASDA', 'ASgDASD@JHG.COM', '2022-08-28', '');
+INSERT INTO clientes (id, nombre, apellido, email, create_at, foto) VALUES(11, 'AAJDHF', 'ASDA', 'ASDfASD@JHG.COM', '2022-08-28', '');
+INSERT INTO clientes (id, nombre, apellido, email, create_at, foto) VALUES(12, 'AAJDHF', 'ASDA', 'ASDAwSD@JHG.COM', '2022-08-28', '');
+INSERT INTO clientes (id, nombre, apellido, email, create_at, foto) VALUES(13, 'AAJDHF', 'ASDA', 'ASDAS4D@JHG.COM', '2022-08-28', '');
+INSERT INTO clientes (id, nombre, apellido, email, create_at, foto) VALUES(14, 'AAJDHF', 'ASDA', 'ASDAS6D@JHG.COM', '2022-08-28', '');
+INSERT INTO clientes (id, nombre, apellido, email, create_at, foto) VALUES(15, 'AAJDHF', 'ASDA', 'ASDAS7D@JHG.COM', '2022-08-28', '');
+INSERT INTO clientes (id, nombre, apellido, email, create_at, foto) VALUES(16, 'AAJDHF', 'ASDA', 'ASDASD@JHG.COM', '2022-08-28', '');
+
+
+INSERT INTO productos (nombre, precio, create_at) VALUES('AKfsdfD', 259000, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('sdfsdfHD', 253000, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('AxcvxcHD', 39000, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('werrwSHD', 559000, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('ghjhgjSHD', 250000, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('zoxiuc', 29000, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('lkqwej', 9000, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('zuyxtc', 20000, NOW());
